adf6c82cb440d8a353d6b8ee2ab519e3d3e49d02
jeffkrantz <jjkrantz@gmail.com>
2017-07-19 12:16:39 -0500

    array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {Http, Response } from '@angular/http';
+
+class Hero{
+  id: number;
+  name: string;
+  power: string;
+  secret_ident:  string;
+  nemesis: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  myHero: Hero = new Hero();
+  heroes: Hero[] = [{id: 1, name: "spidey", power: "web", secret_ident: "test", nemesis: "goblin"}];
+
+  constructor(private http: Http){
+    //grab all heroes from the API and set them to our heroes array
+    this.getHeroes();
+  }
+
+
+  onSubmit(){
+    console.log(this.myHero);
+  }
 }
