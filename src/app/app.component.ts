b6162a4dd45334cc2c99bef4ebfb2a01bea04419
jeffkrantz <jjkrantz@gmail.com>
2017-07-19 15:18:58 -0500

    crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,46 @@ class Hero{
 })
 
 export class AppComponent {
-  myHero: Hero = new Hero();
-  heroes: Hero[] = [{id: 1, name: "spidey", power: "web", secret_ident: "test", nemesis: "goblin"}];
+  newHero: Hero = new Hero();
+  currentHero: Hero = new Hero();
+  heroes: Hero[] = [];
+  baseApiUrl: string = 'http://172.20.0.143:4567/heroes/';
 
   constructor(private http: Http){
     //grab all heroes from the API and set them to our heroes array
     this.getHeroes();
   }
 
+  getHeroes(){
+    this.http.get(this.baseApiUrl).subscribe(response =>
+      this.heroes = response.json()
+    )
+  }
+
+  postHero(){
+    this.http.post(this.baseApiUrl, this.newHero).subscribe(response =>
+      this.heroes.push(response.json())
+    )
+  }
+
+  patchHero(){
+    // localhost:9393/heroes/1
+    this.http.patch(this.baseApiUrl + this.currentHero.id, this.currentHero).subscribe(response =>
+      this.heroes = response.json()
+    )
+  }
+
+  destroyHero(hero){
+    this.http.delete(this.baseApiUrl + hero.id).subscribe(response =>
+      this.heroes = response.json()
+    )
+  }
+
+  editHero(hero){
+    this.currentHero = Object.assign({},hero);
+  }
 
   onSubmit(){
-    console.log(this.myHero);
+    console.log(this.newHero);
   }
 }
